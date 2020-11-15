848de65528220ecea6c2b0b8348cdc066c2c433c
PaulCaa <pablo.caamano@hotmail.com>
2020-11-15 13:38:49 -0300

    DTO in document controller

--- Models/DTO/DocumentoDTO.cs
+++ Models/DTO/DocumentoDTO.cs
@@ -9,16 +9,16 @@ namespace DocumentosOnlineAPI.Models.DTO {
         public string Numero { set; get; }
         public string Fecha { set; get; }
         public string ImgPath { set; get; }
-        public int EmpresaId { set; get; }
-        public int SectorId { set; get; }
+        public string Empresa { set; get; }
+        public string Sector { set; get; }
 
         public override string ToString(){
             return "{\"DocumentoId\": \"" + DocumentoId + "\", "
             + "\"Numero\": \"" + Numero + "\", "
             + "\"Fecha\": \"" + Fecha + "\", "
             + "\"ImgPath\": \"" + ImgPath + "\", "
-            + "\"EmpresaId\": \"" + EmpresaId + "\", "
-            + "\"SectorId\": \"" + SectorId + "\"}";
+            + "\"Empresa\": \"" + Empresa + "\", "
+            + "\"Sector\": \"" + Sector + "\"}";
         }
     }
 }
